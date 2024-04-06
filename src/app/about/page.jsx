a2187f91f482b77d0ec8ab7bf71e3dd591563770
ebukathedev <ebuskelele@gmail.com>
2024-04-06 01:00:20 +0100

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,44 @@
+export default function About() {
+	return (
+		<div className="p-3 max-w-6xl mx-auto space-y-4">
+			<h1 className="font-medium text-2xl text-amber-600">About</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Quibusdam quae sed pariatur voluptatibus. Officia facilis nemo
+				voluptates inventore magnam, quibusdam delectus illum. Soluta
+				ipsa quo impedit ex aliquid?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Voluptatibus adipisci, possimus atque, animi obcaecati veritatis
+				accusamus deserunt similique temporibus cupiditate laborum
+				libero, enim consequatur recusandae odit quisquam. Culpa
+				molestiae doloremque nobis repudiandae pariatur ut architecto
+				voluptatibus omnis obcaecati tenetur quae dolorem cupiditate
+				odit quas placeat possimus perferendis commodi dolores,
+				consequatur maiores. Molestias quod est laborum accusamus qui,
+				odit labore non iusto voluptatem itaque velit neque
+				exercitationem illum sed ipsa quasi eius voluptatum facere!
+				Distinctio, dolorum.
+			</p>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+				Molestiae sed molestias earum laborum veniam totam aut quo
+				dolorem alias! Molestiae id nemo voluptate facere quia earum
+				voluptas placeat vitae sapiente maiores reiciendis illum,
+				mollitia provident optio deserunt, dolor enim ducimus nulla.
+				Cupiditate a numquam pariatur aliquam. Hic aperiam iure aut
+				architecto placeat molestias adipisci sed voluptas nihil,
+				expedita modi soluta aliquam totam perferendis. Corporis modi
+				aut saepe natus maxime distinctio veritatis officia eius placeat
+				hic! Distinctio libero pariatur inventore perspiciatis
+				recusandae? Tenetur dolor at molestias odio similique dolore
+				vero rerum unde illo soluta praesentium, velit saepe corporis et
+				blanditiis asperiores quidem itaque illum porro fugiat, modi
+				quaerat totam nobis deserunt! Id et iste perspiciatis unde
+				voluptatem impedit ut placeat, consequatur, architecto eos
+				magnam similique.
+			</p>
+		</div>
+	);
+}
